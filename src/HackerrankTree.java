c4bb481bf32dbab57eb195a8a0d10044f89bc902
Student101 <student101@PSAdmins-Air-3.home>
2020-04-21 00:08:52 -0400

    first commit

---
+++ src/HackerrankTree.java
@@ -0,0 +1,19 @@
+public class HackerrankTree {
+
+//    int height(Node* root) {
+//        // Write your code here.
+//
+//        if (!root || (!root->left && !root->right)){
+//            return 0;
+//        }
+//        else{
+//
+//            return 1+max(height(root->left),height(root->right));
+//
+//        }
+//
+//    }
+
+
+    
+}
